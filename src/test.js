d25666539dad1e471887615eafaf3ac37bdb9ed8
Tien8129 <tien08012009@gmail.com>
2025-03-05 10:22:51 +0700

    Add files via upload

---
+++ src/test.js
@@ -0,0 +1,44 @@
+var testData = {
+    listProduct: [
+        {
+            productName: 'Loading...',
+            productImg: 'Loading...',
+            productQuanity: 'Loading...',
+            items: [
+                {
+                    itemName: 'Loading...',
+                    itemQuanity: 0,
+                    price: 0,
+                },
+                {
+                    itemName: 'Loading...',
+                    itemQuanity: 0,
+                    price: 0,
+                },
+                {
+                    itemName: 'Loading...',
+                    itemQuanity: 0,
+                    price: 0,
+                },
+            ],
+        },
+    ],
+    listCost: [
+        {
+            costName: 'Loading...',
+            costValue: 0,
+        },
+        {
+            costName: 'Loading...',
+            costValue: 0,
+        },
+    ],
+    listPromotion: [
+        {
+            promotionName: 'giảm giá',
+            promotionValue: 0,
+        },
+    ],
+};
+
+export default testData;
